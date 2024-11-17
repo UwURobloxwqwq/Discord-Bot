9e3257920b84c2532d9a7f47199166db2aa7ff07
UwURobloxwqwq <144137727+UwURobloxwqwq@users.noreply.github.com>
2024-11-17 22:12:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
+import hashlib
+from keep_alive import keep_alive
+keep_alive()
 
-intents = discord.Intents.default()
-intents.messages = True  # For receiving messages
-intents.guilds = True    # For server-specific commands
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
+bot = commands.Bot(command_prefix="!", intents=nextcord.Intents.all(), case_insensitive=True)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
-    print('------')
+  print("————————————————————————")
+  print(f"{bot.user} is online :)")
+  print("————————————————————————")
 
-@bot.event
-async def on_message(message):
-    if bot.user.mentioned_in(message):
-        await message.channel.send('Hello! I am a bot of The God Empire, bestowed with intents!')
-    await bot.process_commands(message)
+@bot.command(pass_context = True)
+async def gen_fluxus(ctx):
+  embed = nextcord.Embed(title="✅️ Key Generated Successfully! ✅️", color=nextcord.Color.green())
+  embed.add_field(name="Gen key:", value="https://flux.li/android/external/start.php?HWID=" + 
+hashlib.sha256(b"Nobody inspects the spammish repetition").hexdigest())
+  embed.set_footer(text=f"Sent by: {ctx.author.name}")
+  await ctx.reply(embed=embed)
 
+@bot.command(pass_context = True)
+async def gen_delta(ctx):
+  embed = nextcord.Embed(title="✅️ Key Generated Successfully! ✅️", color=nextcord.Color.green())
+  embed.add_field(name="Gen key:", value="https://gateway.platoboost.com/a/8?id=" + 
+hashlib.sha256(b"Nobody inspects the spammish repetition").hexdigest())
+  embed.set_footer(text=f"Sent by: {ctx.author.name}")
+  await ctx.reply(embed=embed)
 
-bot.run('your_token_here')
+@bot.command(pass_context = True)
+async def gen_deltaios(ctx):
+  embed = nextcord.Embed(title="✅️ Key Generated Successfully! ✅️", color=nextcord.Color.green())
+  embed.add_field(name="Gen key:", value="https://gateway.platoboost.com/a/2?id=" + 
+hashlib.sha256(b"Nobody inspects the spammish repetition").hexdigest())
+  embed.set_footer(text=f"Sent by: {ctx.author.name}")
+  await ctx.reply(embed=embed)
+
+bot.run('MTE2NzY4MjE0ODExMTc0OTIwMA.G7Q9_4.8QXNbDg47ZIajNaxfyRvcyMGN3m3-fPjHt7NBw')
